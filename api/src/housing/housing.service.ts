0285e7075c9a2f925943abd35c7a34a87d82c8ca
Arthur Wang <75041710+aardvark-warrior@users.noreply.github.com>
2024-02-21 18:16:33 -0500

    Implement findAll and findOne in housing service

--- api/src/housing/housing.service.ts
+++ api/src/housing/housing.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Housing } from './housing.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class HousingService {}
+export class HousingService {
+  constructor(
+    @InjectRepository(Housing)
+    private housingRepository: Repository<Housing>,
+  ) {}
+  async findAll(
+    limit: number,
+    offset: number,
+    search?: string,
+  ): Promise<Housing[]> {
+    const queryBuilder = this.housingRepository.createQueryBuilder('housings');
+    
+    queryBuilder.limit(limit);
+    queryBuilder.offset(offset);
+    queryBuilder.orderBy("housings.name", "ASC");
+
+    let hasWhereCondition = false;
+
+    if (search !== undefined) {
+      queryBuilder.where("housings.name ILIKE :search", {
+        search: `%${search}%`
+      });
+      hasWhereCondition = true;
+    }
+
+    return queryBuilder.getMany();
+  }
+
+  async findOne(id: string): Promise<Housing | null> {
+    return this.housingRepository.findOne({ where: {id}, });
+  }
+}
