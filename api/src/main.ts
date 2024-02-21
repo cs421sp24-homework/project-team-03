4d438f544292d3bb22686f428dc3285abbf68bd2
Arthur Wang <75041710+aardvark-warrior@users.noreply.github.com>
2024-02-21 18:10:47 -0500

    Implement hTTP interceptors to standardize HTTP responses

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+<<<<<<< HEAD
 import { HttpResponseFilter } from './guards/http-response.filter';
 import { HttpResponseInterceptor } from './interceptors/http-response.interceptor';
 import { ValidationPipe } from "@nestjs/common";
+=======
+import { ValidationPipe } from '@nestjs/common';
+import { HttpResponseInterceptor } from './interceptors/http-reponse.interceptor';
+>>>>>>> 754ce3d (Implement hTTP interceptors to standardize HTTP responses)
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -21,9 +26,13 @@ async function bootstrap() {
       },
     }),
   );
+<<<<<<< HEAD
   app.useGlobalFilters(new HttpResponseFilter());
   app.useGlobalInterceptors(new HttpResponseInterceptor());
 
+=======
+  app.useGlobalInterceptors(new HttpResponseInterceptor()); // <-- Add this line
+>>>>>>> 754ce3d (Implement hTTP interceptors to standardize HTTP responses)
   await app.listen(3000);
 }
 bootstrap();
