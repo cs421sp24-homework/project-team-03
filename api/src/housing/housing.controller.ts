0ba7fc02b5468abd721dc409e54ac6a6e62fc98f
Arthur Wang <75041710+aardvark-warrior@users.noreply.github.com>
2024-02-22 11:27:39 -0500

    Implement findAll and findOne methods for housing controller

--- api/src/housing/housing.controller.ts
+++ api/src/housing/housing.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Param, Query } from '@nestjs/common';
+import { HousingService } from './housing.service';
+import { FindHousingsQueryDTO } from './find-housings-query.dto';
+import { FindHousingsResponseDTO } from './find-housings-reponse.dto';
+import { HousingResponseDTO } from './housing-reponse.dto';
 
 @Controller('housing')
-export class HousingController {}
+export class HousingController {
+  constructor(private readonly housingService: HousingService) {}
+
+  @Get()
+  async findAll(
+    @Query() query: FindHousingsQueryDTO,
+  ): Promise<FindHousingsResponseDTO> {
+    const { limit, offset, search } = query;
+
+    const housings = await this.housingService.findAll(
+      limit,
+      offset,
+      search,
+    );
+
+    return {
+      limit,
+      offset,
+      search,
+      data: housings.map((housing) => housing as HousingResponseDTO),
+    }
+  }
+
+  @Get(':id')
+  async findOne(
+    @Param('id') id: string,
+  ): Promise<HousingResponseDTO> {
+    const housing = await this.housingService.findOne(id);
+
+    if (!housing) {
+      throw new NotFoundException();
+    }
+
+    return housing;
+  }
+}
