8c6c826e3c7898e63ee31be95159a0c2f47bec57
Arthur Wang <75041710+aardvark-warrior@users.noreply.github.com>
2024-02-22 11:12:55 -0500

    Install class-validator and class-transformer and add validation to find-housings-query DTO

--- api/src/housing/find-housings-query.dto.ts
+++ api/src/housing/find-housings-query.dto.ts
@@ -1,6 +1,19 @@
-export class FindHousingsQueryDTO {
-  limit: number = 10;
-  offset: number = 0;
-  search?: string;
+import { IsInt, IsOptional, IsPositive, IsString, Max, Min } from "class-validator";
 
+export class FindHousingsQueryDTO {
+  @IsInt()
+  @IsPositive()
+  @Min(1)
+  @Max(50)
+  @IsOptional()
+  limit: number = 10;
+
+  @IsInt()
+  @Min(0)
+  @IsOptional()
+  offset: number = 0;
+
+  @IsString()
+  @IsOptional()
+  search?: string;
 }
