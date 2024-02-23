24c3787488401fd2fd42b1148206b97a59e515be
Arthur Wang <75041710+aardvark-warrior@users.noreply.github.com>
2024-02-23 13:47:47 -0500

    Resolve merge conflict

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,13 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-<<<<<<< HEAD
 import { HttpResponseFilter } from './guards/http-response.filter';
 import { HttpResponseInterceptor } from './interceptors/http-response.interceptor';
 import { ValidationPipe } from "@nestjs/common";
-=======
-import { ValidationPipe } from '@nestjs/common';
-import { HttpResponseInterceptor } from './interceptors/http-reponse.interceptor';
->>>>>>> 754ce3d (Implement hTTP interceptors to standardize HTTP responses)
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -26,13 +21,9 @@ async function bootstrap() {
       },
     }),
   );
-<<<<<<< HEAD
   app.useGlobalFilters(new HttpResponseFilter());
   app.useGlobalInterceptors(new HttpResponseInterceptor());
 
-=======
-  app.useGlobalInterceptors(new HttpResponseInterceptor()); // <-- Add this line
->>>>>>> 754ce3d (Implement hTTP interceptors to standardize HTTP responses)
   await app.listen(3000);
 }
 bootstrap();
