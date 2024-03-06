6aca9c534a051c585f413339b0445d7dd8074601
sabira-tasneem <stasneem2003@gmail.com>
2024-03-06 00:27:01 -0500

    added routing to the page when clicking on the marker

--- app/src/components/map/map-container.tsx
+++ app/src/components/map/map-container.tsx
@@ -1,30 +1,34 @@
-import { useStore } from '@/lib/store';
-import { GoogleMap, Marker, LoadScript } from '@react-google-maps/api';
+import { GoogleMap, Marker } from "@react-google-maps/api";
+import { useNavigate } from "react-router-dom";
+import { HousingItem } from "@/lib/types";
+import useQueryHousingItems from "@/hooks/use-query-housing-items";
 
 const MapContainer = () => {
-  const MAP_API_KEY = "AIzaSyAu9JTvSmCucLYTHPSX9ryl0RXSPbPbWLQ"; // Replace with your actual API key
-  const housingItems = useStore((state) => state.housingItems);
+  const { housingItems } = useQueryHousingItems();
+  const navigate = useNavigate();
+
+  const handleMarkerClick = (item: HousingItem) => {
+    navigate(`/housings/${item.id}`);
+  };
 
   return (
-    <LoadScript googleMapsApiKey={MAP_API_KEY}>
-      <GoogleMap
-        mapContainerStyle={{ height: "400px", width: "800px" }}
-        center={{ lat: 39.330420, lng: -76.618050 }} // Default center if no housing items
-        zoom={13} // Initial zoom level
-      >
-        {housingItems.map((item, index) => (
-        // Check if latitude and longitude are defined
-        item.latitude !== undefined && item.longitude !== undefined && (
-            <Marker
+    <GoogleMap
+      mapContainerStyle={{ height: "400px", width: "800px" }}
+      center={{ lat: 39.330420, lng: -76.618050 }}
+      zoom={13}
+    >
+      {housingItems.map((item: HousingItem, index: number) => (
+        item.latitude !== undefined &&
+        item.longitude !== undefined && (
+          <Marker
             key={index}
             position={{ lat: item.latitude, lng: item.longitude }}
-            />
+            onClick={() => handleMarkerClick(item)}
+          />
         )
-        ))}
-      </GoogleMap>
-    </LoadScript>
+      ))}
+    </GoogleMap>
   );
 };
 
 export default MapContainer;
-
