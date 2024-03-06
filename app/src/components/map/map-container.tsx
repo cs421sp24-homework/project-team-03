b6bb034b178f47a65bfb501474e1389884fc85ad
sabira-tasneem <stasneem2003@gmail.com>
2024-03-06 00:27:01 -0500

    added routing to the page when clicking on the marker

--- app/src/components/map/map-container.tsx
+++ app/src/components/map/map-container.tsx
@@ -1,25 +1,33 @@
 import { useStore } from '@/lib/store';
 import { GoogleMap, Marker, LoadScript } from '@react-google-maps/api';
+import { useNavigate } from 'react-router-dom';
+import { HousingItem } from '@/lib/types';
 
 const MapContainer = () => {
-  const MAP_API_KEY = "AIzaSyAu9JTvSmCucLYTHPSX9ryl0RXSPbPbWLQ"; // Replace with your actual API key
+  const MAP_API_KEY = "AIzaSyAu9JTvSmCucLYTHPSX9ryl0RXSPbPbWLQ"; 
   const housingItems = useStore((state) => state.housingItems);
+  const navigate = useNavigate();
+
+  const handleMarkerClick = (item: HousingItem) => {
+    navigate(`/housings/${item.id}`); // Navigate to housing details page
+  };
 
   return (
     <LoadScript googleMapsApiKey={MAP_API_KEY}>
       <GoogleMap
         mapContainerStyle={{ height: "400px", width: "800px" }}
-        center={{ lat: 39.330420, lng: -76.618050 }} // Default center if no housing items
-        zoom={13} // Initial zoom level
+        center={{ lat: 39.330420, lng: -76.618050 }}
+        zoom={13}
       >
         {housingItems.map((item, index) => (
-        // Check if latitude and longitude are defined
-        item.latitude !== undefined && item.longitude !== undefined && (
+          // Check if latitude and longitude are defined
+          item.latitude !== undefined && item.longitude !== undefined && (
             <Marker
-            key={index}
-            position={{ lat: item.latitude, lng: item.longitude }}
+              key={index}
+              position={{ lat: item.latitude, lng: item.longitude }}
+              onClick={() => handleMarkerClick(item)} // Handle click event
             />
-        )
+          )
         ))}
       </GoogleMap>
     </LoadScript>
@@ -27,4 +35,3 @@ const MapContainer = () => {
 };
 
 export default MapContainer;
-
