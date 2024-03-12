4f804246bd631ba6d299ef6046ecb2d175450d90
EmilyEljamal <eeljama1@jh.edu>
2024-03-12 18:17:51 -0400

    posts e2e testing

---
+++ app/cypress/e2e/housing-app-e2e/post-functions-spec.cy.ts
@@ -0,0 +1,157 @@
+const generateRandomPassword = (length = 8) => {
+    const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()';
+    let password = '';
+    for (let i = 0; i < length; i++) {
+        password += chars.charAt(Cypress._.random(0, chars.length - 1));
+    }
+    return password;
+  }
+  
+  const generateRandomName = (length = 6) => {
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    let name = '';
+    for (let i = 0; i < length; i++) {
+        name += letters.charAt(Cypress._.random(0, letters.length - 1));
+    }
+    return name;
+  }
+
+  const generateRandomCost = (length = 4) => {
+    const numbers = '123456789';
+    let cost = '';
+    for (let i = 0; i < length; i++) {
+        cost += numbers.charAt(Cypress._.random(0, numbers.length - 1));
+    }
+    return cost;
+  }
+
+describe('test post functionality', () => {
+    let randomName;
+    let randomEmail;
+    let randomPassword;
+    let randomTitle;
+    let randomContent;
+    let randomAddress;
+    let randomCost
+
+    before(() => {
+        // Generate the credentials before all tests run
+        randomName = generateRandomName();
+        randomEmail = randomName + "@jhu.edu";
+        randomPassword = generateRandomPassword(10);
+
+    })
+
+    beforeEach(() => {
+        cy.visit('http://localhost:5173/project-team-03/');
+        cy.contains('Off-Campus Housing').should('be.visible');
+        randomTitle = generateRandomName();
+        randomContent = generateRandomName(12);
+        randomAddress = generateRandomName(12);
+        randomCost = generateRandomCost();
+    });
+
+
+    it('Feed loads when navigating to posts', () => {
+        cy.registerUser(randomEmail, randomPassword, randomName, randomName);
+        cy.loginUser(randomEmail, randomPassword);
+        cy.get('#see-posts').click();
+        cy.contains('Community Posts').should('be.visible');
+        cy.url().should('include', '/posts');
+    })
+
+    it('Seeing Community posts fails when user isnt logged in', () => {
+        cy.get('#see-posts').click();
+        cy.get('#toast', { timeout: 1000 })
+            .should('exist')
+            .contains(/log in to view posts/i);
+    })
+
+    it('Adds post successfully', () => {
+        cy.loginUser(randomEmail, randomPassword);
+        cy.get('#see-posts').click();
+        cy.get('#add-posts').click();
+
+        cy.get('#type').select('Roommate');
+        cy.get('#title').type(randomTitle);
+        cy.get('#content').type(randomContent);
+        cy.get('#cost').type(randomCost);
+        cy.get('#address').type(randomAddress);
+        cy.contains('Submit').click();
+
+        cy.contains(randomTitle).should('be.visible');
+        cy.contains(randomContent).should('be.visible');
+        cy.contains(randomCost).should('be.visible');
+        cy.contains(randomAddress).should('be.visible');
+    })
+
+    it('Deletes post successfully', () => {
+        cy.loginUser(randomEmail, randomPassword);
+        cy.get('#see-posts').click();
+        cy.get('#add-posts').click();
+
+        cy.get('#type').select('Roommate');
+        cy.get('#title').type(randomTitle);
+        cy.get('#content').type(randomContent);
+        cy.get('#cost').type(randomCost);
+        cy.get('#address').type(randomAddress);
+        cy.contains('Submit').click();
+
+        cy.contains(randomTitle).should('be.visible');
+        cy.contains(randomContent).should('be.visible');
+        cy.contains(randomCost).should('be.visible');
+        cy.contains(randomAddress).should('be.visible');
+
+        cy.contains(randomTitle).parent().find('#delete-post').click({ force: true });
+        cy.get('#delete-btn').click({ force: true });
+
+        cy.contains(randomTitle).should('not.exist');
+        cy.contains(randomContent).should('not.exist');
+        cy.contains(randomCost).should('not.exist');
+        cy.contains(randomAddress).should('not.exist');
+    })
+
+    it('Adding a post fails when a field is missing', () => {
+        cy.loginUser(randomEmail, randomPassword);
+        cy.get('#see-posts').click();
+        cy.get('#add-posts').click();
+
+        cy.get('#type').select('Roommate');
+        cy.get('#content').type(randomContent);
+        cy.get('#cost').type(randomCost);
+        cy.get('#address').type(randomAddress);
+        cy.contains('Submit').click();
+
+        cy.get('#toast', { timeout: 1000 })
+            .should('exist')
+            .contains(/must be completed/i);
+
+        cy.contains(randomContent).should('not.exist');
+        cy.contains(randomCost).should('not.exist');
+        cy.contains(randomAddress).should('not.exist');
+    })
+
+    it('Adding a post fails when the cost isnt numerical', () => {
+        cy.loginUser(randomEmail, randomPassword);
+        cy.get('#see-posts').click();
+        cy.get('#add-posts').click();
+
+        randomCost = generateRandomName(3);
+
+        cy.get('#type').select('Roommate');
+        cy.get('#title').type(randomTitle);
+        cy.get('#content').type(randomContent);
+        cy.get('#cost').type(randomCost);
+        cy.get('#address').type(randomAddress);
+        cy.contains('Submit').click();
+
+        cy.get('#toast', { timeout: 1000 })
+            .should('exist')
+            .contains(/must be a valid number/i);
+
+        cy.contains(randomTitle).should('not.exist');
+        cy.contains(randomContent).should('not.exist');
+        cy.contains(randomCost).should('not.exist');
+        cy.contains(randomAddress).should('not.exist');
+    })
+})
--- app/src/components/post/add-post-dialog.tsx
+++ app/src/components/post/add-post-dialog.tsx
@@ -69,7 +69,7 @@ export const AddPostDialog = () => {
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button aria-label={"Make a Post"} variant="default" size="sm">
+        <Button id="add-posts" aria-label={"Make a Post"} variant="default" size="sm">
           <PlusCircledIcon className="w-5 h-5" />
         </Button>
       </DialogTrigger>
@@ -95,9 +95,9 @@ export const AddPostDialog = () => {
                 }}
               >
                 <option value="">Select a post type...</option>
-                <option value="Roommate">Looking for Roommate</option>
-                <option value="Sublet">Looking for Subletter</option>
-                <option value="Housing">Looking for Housing</option>
+                <option id="Roommate" value="Roommate">Looking for Roommate</option>
+                <option id="Sublet" value="Sublet">Looking for Subletter</option>
+                <option id="Housing" value="Housing">Looking for Housing</option>
               </select>
             </div>
             {/* Textarea and other form fields */}
@@ -162,6 +162,7 @@ export const AddPostDialog = () => {
                 <div>(Please enter an integer)</div>
               </Label>
               <Input
+                id="cost"
                 type="number"
                 onChange={(e) => setCost(Number(e.target.value))}
               />
