174f8aee95aa3d9766ee1eaabd9cf8319bbc7f0b
Kai <kholton3@jhu.edu>
2024-04-14 15:01:06 -0400

    Updating from desktop to laptop

--- api/src/favorite-housing/favorite-housing.controller.ts
+++ api/src/favorite-housing/favorite-housing.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Param, UseGuards } from '@nestjs/common';
+import { Controller, Param, Post, UseGuards } from '@nestjs/common';
 import { FavoriteHousingService } from './favorite-housing.service';
 import { HousingExistsGuard } from 'src/guards/housing-exists.guard';
 import { JwtAuthGuard } from 'src/guards/jwt-auth.guard';
@@ -25,5 +25,73 @@ export class FavoriteHousingController {
     delete favorite_housing.userId;
     return favorite_housing;
   }
-  
+ 
+  @Get(':housingId')
+  async findOne(
+    @Param('reviewId') id: string,
+    @Param('housingId') housingId: string,
+  ): Promise<ReviewResponseDto> {
+    const review = await this.reviewsService.findOne(id, housingId);
+    if (!review) {
+      throw new NotFoundException(
+        `Review with ID ${id} not found in housing item with ID ${housingId}`,
+      );
+    }
+    delete review.userId;
+    return review;
+  }
+
+  @UseGuards(ReviewOwnershipGuard)
+  @UseGuards(JwtAuthGuard)
+  @Delete(':reviewId')
+  async remove(
+    @Param('reviewId') reviewId: string,
+    @Param('housingId') housingId: string,
+  ): Promise<{ statusCode: number; message: string }> {
+    const review = await this.reviewsService.remove(reviewId, housingId);
+    if (!review) {
+      throw new NotFoundException(
+        `Review with ID ${reviewId} not found in housing item with ID ${housingId}`,
+      );
+    }
+
+    // call housingService aggregate review
+
+    return {
+      statusCode: 200,
+      message: 'Review deleted successfully',
+    };
+  }
+
+  @UseGuards(HousingExistsGuard)
+  @Get()
+  async findAll(
+    @Param('housingId') housingId: string,
+    @Query() query: FindReviewsQueryDTO,
+  ): Promise<FindReviewsResponseDTO> {
+    const { limit, offset, search, sortBy, withUserData } = query;
+
+    const reviews = await this.reviewsService.findAll(
+      limit,
+      offset,
+      housingId,
+      search,
+      sortBy,
+      withUserData,
+    );
+
+    return {
+      limit,
+      offset,
+      search,
+      withUserData,
+      data: reviews.map((review) => {
+        delete review.userId;
+        if (review.user) {
+          delete review.user.password;
+        }
+        return review;
+      }),
+    };
+  }
 }
