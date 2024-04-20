9fde0ffc0352181551a10221fd5a86005b9358b4
Arthur Wang <75041710+aardvark-warrior@users.noreply.github.com>
2024-04-20 19:27:30 -0400

    Fix favorite-housing/post spec files

--- api/src/favorite-housing/favorite-housing.controller.spec.ts
+++ api/src/favorite-housing/favorite-housing.controller.spec.ts
@@ -3,19 +3,58 @@ import { FavoriteHousingController } from './favorite-housing.controller';
 import { FavoriteHousingService } from './favorite-housing.service';
 import { FavoriteHousingModule } from './favorite-housing.module';
 import { HousingService } from 'src/housing/housing.service';
+import { Repository } from 'typeorm';
+import { favoriteHousing } from './favorite-housing.entity';
+import { Housing } from 'src/housing/housing.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { RouteParamMetadata } from '@nestjs/common';
 
 describe('FavoriteHousingController', () => {
+  let controller: FavoriteHousingController;
   let service: FavoriteHousingService;
-  let housingssService: HousingService;
+  let housingService: HousingService;
+  let repository: Repository<favoriteHousing>;
+  let housingRepository: Repository<Housing>;
+
+  const FAV_HOUSING_REPO_TOKEN = getRepositoryToken(favoriteHousing);
+  const HOUSING_REPO_TOKEN = getRepositoryToken(Housing);
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [FavoriteHousingModule],
       controllers: [FavoriteHousingController],
-      providers: [FavoriteHousingService, HousingService]
+      providers: [
+        FavoriteHousingService, 
+        HousingService,
+        {
+          provide: FAV_HOUSING_REPO_TOKEN,
+          useValue: {
+            create: jest.fn(),
+            findOneBy: jest.fn(),
+            createQueryBuilder: jest.fn(),
+            save: jest.fn(),
+            preload: jest.fn(),
+            remove: jest.fn(),
+          },
+        },
+        {
+          provide: HOUSING_REPO_TOKEN,
+          useValue: {
+            create: jest.fn(),
+            findOneBy: jest.fn(),
+            createQueryBuilder: jest.fn(),
+            save: jest.fn(),
+            preload: jest.fn(),
+            remove: jest.fn(),
+          },
+        },
+      ]
     }).compile();
 
-    service = module.get<FavoriteHousingService>(FavoriteHousingService)
+    controller = module.get<FavoriteHousingController>(FavoriteHousingController);
+    service = module.get<FavoriteHousingService>(FavoriteHousingService);
+    housingService = module.get<HousingService>(HousingService);
+    repository = module.get<Repository<favoriteHousing>>(FAV_HOUSING_REPO_TOKEN);
+    housingRepository = module.get<Repository<Housing>>(HOUSING_REPO_TOKEN);
     
   });
 
